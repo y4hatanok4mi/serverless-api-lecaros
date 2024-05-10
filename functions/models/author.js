48a4e2ddd5474d7391d0a86fa792968cd9f8be1e
y4hatanok4mi <lecarosjc@gmail.com>
2024-05-10 22:03:25 +0800

    update

--- functions/models/author.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const authorSchema = require("../schema/author");
-
-const AuthorModel = mongoose.model("Author", authorSchema);
-
-module.exports = AuthorModel;
